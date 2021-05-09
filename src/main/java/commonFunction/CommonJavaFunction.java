d411d788e12b1fe9328dbde39dff2bcf3bc59916
nehachopra27 <neha27chopra@gmail.com>
2021-05-09 21:04:21 +0100

    Delete src/main/java directory

--- src/main/java/commonFunction/CommonJavaFunction.java
+++
@@ -1,9 +0,0 @@
-package commonFunction;
-
-import java.io.IOException;
-import java.util.List;
-
-public interface CommonJavaFunction {
-
-	public boolean writedata(List<String[]> dataset) throws IOException;
-}
