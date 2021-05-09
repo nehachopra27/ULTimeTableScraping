c04cd48bd975aa9fb0508d1c00dd4e17fb799650
nehachopra27 <neha27chopra@gmail.com>
2021-05-10 03:58:02 +0530

    Add files via upload

--- LectureCount.py
+++ LectureCount.py
@@ -7,7 +7,7 @@ spark = SparkSession.builder.appName("CS6502").getOrCreate()
 
 timetables = spark.read.format("csv") \
     .option("header", True) \
-    .load("/home/g20084447/ULTimeTableScraping/src/test/resources/dataset.csv",inferSchema=True)
+    .load("file:///home/g20084447/ULTimeTableScraping/src/test/resources/dataset.csv",inferSchema=True)
 
 timetables=timetables.filter(timetables['Type']=="LEC").select(timetables['Total Classes in Semester']).groupBy().sum().alias('CountLectures')
 timetables.show()
